c550e22c61fcb2397a6f672df9061fa49c769624
David Zou <davidzsy20@Davids-MBP.fios-router.home>
2018-01-09 15:04:13 -0500

    new branch

--- jump/server.js
+++ jump/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-const webpack = require('webpack');
-const axios = require('axios');
-const bodyParser = require('body-parser') ;
-const path = require('path');
+// const webpackDevMiddleware = require('webpack-dev-middleware');
+// const webpack = require('webpack');
+// const axios = require('axios');
+// const bodyParser = require('body-parser') ;
+// const path = require('path');
 const app = express();
 const server = app.listen(3000, function() {
     const host = server.address().address;
@@ -11,13 +11,13 @@ const server = app.listen(3000, function() {
     console.log('Example app listening at http://%s:%s', host, port);
   });
 
-app.use(partial());
+// app.use(partial());
 // Parse JSON (uniform resource locators)
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
 // Parse forms (signup/login)
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + '/src'));
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.static(__dirname + '/src'));
 app.use(cookieParser());
